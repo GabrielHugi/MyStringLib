f79660d92a544f68d58e103d0677c94c651883d5
Gabriel Loureiro Amorim Hugi <gabrielhugi2@gmail.com>
2024-07-26 15:38:48 +0000

    Header file.

---
+++ mystring.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+extern size_t mystrlen (const char *str);
+extern int removefgetsnewline (char *str);
+extern char *mystrcpy (char* str, const char* copy);
+extern int mystrncpy (char* str, const char* copy, size_t d);
+extern int mystrcat (char *str, const char *cat);
+extern int mystrncat (char *str, const char *cat, size_t d);
+extern int mystrcmp (const char *str, const char *comp);
+extern int mystrncmp (const char *str, const char *comp, size_t d);
+extern char *mystrchr (const char *str, int b);
+extern char *mystrrchr (const char *str, int b);
+extern char *mystrstr (const char *str, const char *substr);
+extern size_t mystrcspn (const char *str, const char *str2);
+extern size_t mystrspn (const char *str, const char *str2);
